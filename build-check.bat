8a98db1058be1f92042f2cbc4ac4afe214ccd500
andrevitor89-eng <andrevitor89-eng@users.noreply.github.com>
2026-06-30 08:02:47 -0300

    chore: repositorio 100% frontend (remove utilitarios, docker-compose, docs e exemplos)

--- build-check.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "%~dp0apps\web"
-echo === npm run build (igual ao da Vercel) === > "%~dp0build-check.log"
-call npm run build >> "%~dp0build-check.log" 2>&1
-echo BUILD_EXIT=%errorlevel% >> "%~dp0build-check.log"
-echo DONE>> "%~dp0build-check.log"
